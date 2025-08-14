851aebcb585c868f89a7154461200491b6279dd2
Festus Yeboah <festus.yeboah@konux.de>
2025-08-14 09:25:57 +0200

    more fixes

--- apps/plugin-react/vite.config.ts
+++ apps/plugin-react/vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 // https://chatgpt.com/share/e/689ca8ff-fdcc-8005-8414-b2d6e97d0d71
 export default defineConfig({
   plugins: [react()],
