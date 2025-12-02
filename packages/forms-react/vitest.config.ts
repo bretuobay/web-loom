94af1b8c925c992c6df58beefbbd3617250d2865
Festus Yeboah <festus.yeboah@konux.de>
2025-12-02 23:22:17 +0100

    New install to clean up packages

--- packages/forms-react/vitest.config.ts
+++ packages/forms-react/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -26,14 +25,6 @@ export default defineConfig({
         'src/**/*.spec.tsx',
         'src/test/**',
       ],
-      threshold: {
-        global: {
-          branches: 80,
-          functions: 80,
-          lines: 80,
-          statements: 80,
-        },
-      },
     },
   },
 });
